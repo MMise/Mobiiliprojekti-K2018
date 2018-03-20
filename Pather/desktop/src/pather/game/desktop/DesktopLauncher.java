bb06bfb95e591e521b1628a754378ce3c209a4a2
Ville-Veikko Komulainen <V-V.Komulainen@hotmail.com>
2018-03-20 12:36:31 +0200

    Desktop launcher lisätty

---
+++ Pather/desktop/src/pather/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pather.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pather.game.Pather;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Pather(), config);
+	}
+}
