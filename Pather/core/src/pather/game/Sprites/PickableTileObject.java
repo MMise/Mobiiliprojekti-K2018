cab796200c2a4d269c6fc0e5b5d886d1b0c7aa48
Mikko Jokinen <mikko.jokinen96@gmail.com>
2018-03-21 11:57:41 +0200

    Added a class for pickable objects

---
+++ Pather/core/src/pather/game/Sprites/PickableTileObject.java
@@ -0,0 +1,27 @@
+package mario.bros.Sprites;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.tiled.TiledMapTileSet;
+
+import mario.bros.MainApp;
+import mario.bros.Screens.PlayScreen;
+
+
+
+public class PickableTileObject extends InteractiveTileObject {
+    private static TiledMapTileSet tileSet;
+    private final int tileIndex = 83; //Tiled tileset ID + 1
+
+    public PickableTileObject(PlayScreen screen, MapObject object) {
+        super(screen, object);
+        tileSet = map.getTileSets().getTileSet("fantasy-tileset"); //Change tileset name
+        fixture.setUserData(this);
+        fixture.setSensor(true);
+        setCategoryFilter(MainApp.TRIGGER_BIT); //Add this short to main class
+    }
+
+    @Override
+    public void onHit(Player player) {
+        getCell().setTile(tileSet.getTile(tileIndex));
+        //doStuff
+    }
+}
