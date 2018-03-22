f7fe4d4527e7da1757d5d8cae40159918a8759cc
Ville-Veikko Komulainen <V-V.Komulainen@hotmail.com>
2018-03-22 12:47:58 +0200

    variable tweaking

--- Pather/core/src/pather/game/Pather.java
+++ Pather/core/src/pather/game/Pather.java
@@ -19,7 +19,7 @@ public class Pather extends Game {
 	public static final short NOTHING_BIT = 0; //
 	public static final short GROUND_BIT = 1;
 	public static final short PLAYER_BIT = 2;
-	public static final short BRICK_BIT = 4;
+	public static final short WIN_BIT = 4;
 	public static final short DANGER_ZONE_BIT = 8;
 	public static final short DESTROYED_BIT = 16;
 	public static final short OBJECT_BIT = 32;
