093d42c493601d4efcf012347dd90828af5fe4f5
MMise <mikko.jokinen96@gmail.com>
2018-03-26 17:57:20 +0300

    remove youtube_tutoriaali

--- youtube_tutoriaali/android/assets/tilesetti.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tilesetti" tilewidth="32" tileheight="32" tilecount="2" columns="2">
- <image source="tilesetti.png" width="64" height="32"/>
-</tileset>
--- youtube_tutoriaali/core/src/com/mygdx/game/utils/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.mygdx.game.utils;
-
-public final class Constants {
-
-    public static final float PPM = 32;
-}
